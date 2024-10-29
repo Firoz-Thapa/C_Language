c36425f31153dc4e2b8f5138aee3a44254572b9d
Firoz Thapa <154414703+Firoz-Thapa@users.noreply.github.com>
2024-10-29 11:11:46 +0200

    filehandling

--- EmbeddedSystem_c.c/W_Tasks/W3_A6/A6_T1.py
+++ EmbeddedSystem_c.c/W_Tasks/W3_A6/A6_T1.py
@@ -1,4 +1,4 @@
-def read_text_file():
+def main():
     print("Program starting.")
     print("This program can read a file.")
     filename = input("Insert filename: ")
@@ -6,13 +6,12 @@ def read_text_file():
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             print(f"#### START \"{filename}\" ####")
-            content = file.read()
-            print(content)
+            print(file.read())
             print(f"#### END \"{filename}\" ####")
     except FileNotFoundError:
-        print(f"File {filename} not found.")
-    
+        print(f"Error: File '{filename}' not found.")
+
     print("Program ending.")
 
 if __name__ == "__main__":
-    read_text_file()
+    main()
