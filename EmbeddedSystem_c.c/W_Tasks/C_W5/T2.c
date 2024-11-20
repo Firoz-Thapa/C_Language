d9012f1148c00e28286ea4ca486c26e473e13ff3
Firoz Thapa <154414703+Firoz-Thapa@users.noreply.github.com>
2024-11-20 09:50:43 +0200

    fixed T2.c

--- EmbeddedSystem_c.c/W_Tasks/C_W5/T2.c
+++ EmbeddedSystem_c.c/W_Tasks/C_W5/T2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "T2.h"
 
-#define TOTAL_BATTERY_CAPACITY 4000.0
+#define TOTAL_BATTERY_CAPACITY 4000.0 
 #define POWER_CONSUMPTION 150.0
 
 int main(void) {
@@ -9,20 +9,18 @@ int main(void) {
     float totalCapacityWh = TOTAL_BATTERY_CAPACITY / 1000.0;
 
     printf("Program starting.\n");
-    printf("Total Battery Capacity: %.2f Wh\n", totalCapacityWh);
-    printf("Average Power Consumption: %.2f W\n", POWER_CONSUMPTION / 1000.0);
+    printf("Battery info:\n");
+    printf(" - Capacity - %.0f mAh\n", TOTAL_BATTERY_CAPACITY);
+    printf(" - Average power consumption - %.3f Wh\n", POWER_CONSUMPTION / 1000.0);
 
-    printf("Enter battery charge percentage (0-100): ");
+    printf("Insert remaining battery charge in percentage: ");
     scanf("%f", &battery.fChargePercentage);
 
     battery.fChargeRatio = battery.fChargePercentage / 100.0;
-
     battery.fRemainingCapacity = battery.fChargeRatio * totalCapacityWh;
     battery.fRemainingHours = battery.fRemainingCapacity / (POWER_CONSUMPTION / 1000.0);
 
-    printf("Charge ratio: %.2f\n", battery.fChargeRatio);
-    printf("Remaining capacity: %.2f Wh\n", battery.fRemainingCapacity);
-    printf("Remaining battery life: %.2f hours\n", battery.fRemainingHours);
+    printf("%.2f hours of battery remaining.\n", battery.fRemainingHours);
 
     printf("Program ending.\n");
     return 0;
