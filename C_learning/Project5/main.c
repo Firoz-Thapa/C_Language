b7e7069f5022a50f0fae6330510b1b2acb900686
Firoz-Thapa <Gyawat.magar@gmail.com>
2024-12-28 23:54:40 +0200

    made some minor modifications

--- C_learning/Project5/main.c
+++ C_learning/Project5/main.c
@@ -4,117 +4,94 @@
 #include <stdlib.h>
 #include <windows.h>
 
-// Macros variable (HEIGHT, WIDTH)
 #define HEIGHT 20
 #define WIDTH 40
 
-// Array to store the coordinates of snake
-// tail (x-axis, y-axis)
 int snakeTailX[100], snakeTailY[100];
-
-// Variable to store the length of the
-// snake's tail
 int snakeTailLen;
-
-// Score and signal flags
 int gameover, key, score;
-
-// Coordinates of snake's head and fruit
 int x, y, fruitx, fruity;
 
-// Function to generate the fruit
-// within the boundary
-void setup() {
-    
-    // Flag to signal the gameover
-    gameover = 0;
+void initializeSnakeTail() {
+    for (int i = 0; i < 100; i++) {
+        snakeTailX[i] = -1;
+        snakeTailY[i] = -1;
+    }
+    snakeTailLen = 0;
+}
 
-    // Initial coordinates of the snake
+void setup() {
+    gameover = 0;
     x = WIDTH / 2;
     y = HEIGHT / 2;
-    
-    // Initial coordinates of the fruit
-    fruitx = rand() % WIDTH;
-    fruity = rand() % HEIGHT;
-    while (fruitx == 0)
+    key = 2;  // Default direction to the right
+
+    do {
         fruitx = rand() % WIDTH;
-
-    while (fruity == 0)
         fruity = rand() % HEIGHT;
+    } while (fruitx == 0 || fruity == 0);
 
-    // Score initialzed
     score = 0;
+    initializeSnakeTail();
 }
 
 void draw() {
     system("cls");
-    
-    // Creating top wall
     for (int i = 0; i < WIDTH + 2; i++)
         printf("-");
     printf("\n");
-    
+
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j <= WIDTH; j++) {
-
-            // Creating side walls with '#'
             if (j == 0 || j == WIDTH)
                 printf("#");
-            
-            // Creating snake's head with 'O'
-            if (i == y && j == x)
+            else if (i == y && j == x)
                 printf("O");
-            
-            // Creating the sanke's food with '*'
             else if (i == fruity && j == fruitx)
                 printf("*");
-            
-            // Creating snake's body with 'o'
             else {
                 int prTail = 0;
                 for (int k = 0; k < snakeTailLen; k++) {
-                    if (snakeTailX[k] == j
-                        && snakeTailY[k] == i) {
+                    if (snakeTailX[k] == j && snakeTailY[k] == i) {
                         printf("o");
                         prTail = 1;
+                        break;
                     }
                 }
                 if (!prTail)
                     printf(" ");
             }
         }
-      printf("\n");
-            
+        printf("\n");
     }
 
     for (int i = 0; i < WIDTH + 2; i++)
         printf("-");
-     printf("\n");
-
-    printf("score = %d", score);
     printf("\n");
+
+    printf("Score = %d\n", score);
     printf("Press W, A, S, D for movement.\n");
-    printf("Press X to quit the game.");
+    printf("Press X to quit the game.\n");
 }
 
 void input() {
     if (kbhit()) {
         switch (tolower(getch())) {
         case 'a':
-            if(key!=2)
-            key = 1;
+            if (key != 2)
+                key = 1;
             break;
         case 'd':
-            if(key!=1)
-            key = 2;
+            if (key != 1)
+                key = 2;
             break;
         case 'w':
-            if(key!=4)
-            key = 3;
+            if (key != 4)
+                key = 3;
             break;
         case 's':
-            if(key!=3)
-            key = 4;
+            if (key != 3)
+                key = 4;
             break;
         case 'x':
             gameover = 1;
@@ -124,7 +101,6 @@ void input() {
 }
 
 void logic() {
-
     int prevX = snakeTailX[0];
     int prevY = snakeTailY[0];
     int prev2X, prev2Y;
@@ -138,7 +114,7 @@ void logic() {
         prevX = prev2X;
         prevY = prev2Y;
     }
-    
+
     switch (key) {
     case 1:
         x--;
@@ -160,32 +136,51 @@ void logic() {
         gameover = 1;
 
     for (int i = 0; i < snakeTailLen; i++) {
-        if (snakeTailX[i] == x && snakeTailY[i] == y)
+        if (snakeTailX[i] == x && snakeTailY[i] == y) {
             gameover = 1;
+            break;
+        }
     }
 
     if (x == fruitx && y == fruity) {
-        fruitx = rand() % WIDTH;
-        fruity = rand() % HEIGHT;
-        while (fruitx == 0)
+        int validPosition = 0;
+        while (!validPosition) {
             fruitx = rand() % WIDTH;
-
-        while (fruity == 0)
             fruity = rand() % HEIGHT;
+
+            validPosition = 1;
+            for (int i = 0; i < snakeTailLen; i++) {
+                if (snakeTailX[i] == fruitx && snakeTailY[i] == fruity) {
+                    validPosition = 0;
+                    break;
+                }
+            }
+        }
+
         score += 10;
-         snakeTailLen++;
+        snakeTailLen++;
     }
 }
 
-void main() {
+void gameOverScreen() {
+    system("cls");
+    printf("\n====================\n");
+    printf("     GAME OVER!     \n");
+    printf("====================\n");
+    printf("Your Score: %d\n", score);
+    printf("Press any key to exit...\n");
+    getch();
+}
 
+void main() {
     setup();
 
     while (!gameover) {
-
         draw();
         input();
         logic();
-        Sleep(33);
+        Sleep(100 - (score / 10)); // Faster as score increases
     }
+
+    gameOverScreen();
 }
